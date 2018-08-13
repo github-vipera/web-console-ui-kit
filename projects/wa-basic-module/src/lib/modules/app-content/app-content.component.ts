851b90de872b16d7e43dd2f500a8ea109c1deda2
Marco Bonati <marco.bonati@vipera.com>
2018-08-13 16:11:41 +0200

    Added first implementation of tabs

---
+++ projects/wa-basic-module/src/lib/modules/app-content/app-content.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
+import { PluginView } from 'web-console-core'
+import { WCOverlayPaneService, WCToasterService } from 'web-console-ui-kit'
+
+export interface NewUserModel {
+  userId?:string,
+  userIdInt?:number, 
+  msisdn?:number, 
+  serial?:number
+}
+
+@Component({
+  selector: 'wa-app-content',
+  styleUrls: [ './app-content.component.scss' ],
+  templateUrl: './app-content.component.html'
+})
+@PluginView("App Content",{
+  iconName: "ico-app-content" 
+})
+export class AppContentComponent implements OnInit {
+
+  constructor(private toaster: WCToasterService, private overlayPaneService: WCOverlayPaneService) {
+  }
+
+  ngOnInit() {
+  }
+
+
+}
--- projects/wa-basic-module/src/lib/wa-basic-module.module.ts
+++ projects/wa-basic-module/src/lib/wa-basic-module.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { UsersListComponent } from './modules/users-list/users-list.component';
+import { AppContentComponent } from './modules/app-content/app-content.component';
+
 import { WebConsoleCoreModule } from 'web-console-core'
 import { WebConsoleUIKitGridModule, WebConsoleUIKitModuleKendoProvider, WebConsoleUIKitCoreModule } from 'web-console-ui-kit'
 
@@ -8,8 +10,8 @@ import { WebConsoleUIKitGridModule, WebConsoleUIKitModuleKendoProvider, WebConso
   imports: [
     WebConsoleCoreModule, WebConsoleUIKitGridModule, WebConsoleUIKitModuleKendoProvider, WebConsoleUIKitCoreModule, FormsModule
   ],
-  entryComponents:[UsersListComponent],
-  declarations: [UsersListComponent],
-  exports: [UsersListComponent, FormsModule]
+  entryComponents:[UsersListComponent, AppContentComponent],
+  declarations: [UsersListComponent, AppContentComponent],
+  exports: [UsersListComponent, AppContentComponent, FormsModule]
 })
 export class WABasicModule { }
