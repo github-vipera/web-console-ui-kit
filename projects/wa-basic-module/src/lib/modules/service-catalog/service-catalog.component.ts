477dda0c993e527149fc2ef35ba44e23f79d5860
Marco Bonati <marco.bonati@vipera.com>
2018-08-14 10:18:13 +0200

    Added new service tree with Kendo Grid

--- projects/wa-basic-module/src/lib/modules/service-catalog/service-catalog.component.ts
+++ projects/wa-basic-module/src/lib/modules/service-catalog/service-catalog.component.ts
@@ -18,10 +18,30 @@ export class ServiceCatalogComponent implements OnInit {
   public sort: SortDescriptor[] = [];
   public groups: GroupDescriptor[] = [];
 
+  /**
+  public data = [
+    { name:"Default", description:"MOTIF Default", type:"Domain", channel:"", domain: "Default", application: "", service: ""},
+      { name:"Vipera", description:"Vipera Default Application", type:"Application", channel:"", domain: "Default", application: "Vipera", service: ""},
+        { name:"Security", description:"Securiry Service", type:"Service", channel:"", domain: "Default", application: "Vipera", service: "Security"},
+          { name:"login", description:"Login Operation", type:"Operation", channel:"JSON", domain: "Default", application: "Vipera", service: "Security"},
+          { name:"logout", description:"Logout Operation", type:"Operation", channel:"JSON", domain: "Default", application: "Vipera", service: "Security"}
+  ]
+   */
+
+  public data = [
+    { name:"login", description:"Login Operation", type:"Operation", channel:"JSON", domain: "Default", application: "Vipera", service: "Security"},
+    { name:"logout", description:"Logout Operation", type:"Operation", channel:"JSON", domain: "Default", application: "Vipera", service: "Security"},
+    { name:"appcheck", description:"App Check Operation", type:"Operation", channel:"JSON", domain: "Default", application: "Vipera", service: "Utility"},
+    { name:"pay", description:"HCE Pay Operation", type:"Operation", channel:"JSON", domain: "Bankart", application: "NLB Pay", service: "Payments"}
+  ]
+  
+
   constructor(private toaster: WCToasterService, private overlayPaneService: WCOverlayPaneService) {
   }
 
   ngOnInit() {
+    this.groups = [{ field: 'domain' },{ field: 'application' },{ field: 'service' } ];
+    this.gridView = process(this.data, { group: this.groups });
   }
 
 
