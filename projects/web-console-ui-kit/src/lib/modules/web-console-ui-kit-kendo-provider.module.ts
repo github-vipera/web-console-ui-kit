15c298ccbfd19cd07d4d882ed7345722cafa327f
Marco Bonati <marco.bonati@vipera.com>
2018-08-23 18:12:54 +0200

    Refactoring

--- projects/web-console-ui-kit/src/lib/modules/web-console-ui-kit-kendo-provider.module.ts
+++ projects/web-console-ui-kit/src/lib/modules/web-console-ui-kit-kendo-provider.module.ts
@@ -4,7 +4,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GridModule, BodyModule, ExcelModule, FilterMenuModule, FooterModule, GroupModule, HeaderModule } from '@progress/kendo-angular-grid';
 import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
 import { LayoutModule, PanelBarModule, SplitterModule, TabStripModule } from '@progress/kendo-angular-layout';
-
+import { ButtonGroupModule, ButtonModule, ButtonsModule  } from '@progress/kendo-angular-buttons'
+import { CalendarModule, DatePickerModule, DateRangeModule, TimePickerModule, MultiViewCalendarModule, DateInputModule, DateInputsModule, CalendarsModule } from '@progress/kendo-angular-dateinputs';
 
 @NgModule({
     imports:[ 
@@ -12,14 +13,18 @@ import { LayoutModule, PanelBarModule, SplitterModule, TabStripModule } from '@p
         ToolBarModule, 
         GridModule, BodyModule, ExcelModule, FilterMenuModule, FooterModule, GroupModule, HeaderModule, 
         DropDownsModule,
-        LayoutModule, PanelBarModule, SplitterModule, TabStripModule
+        LayoutModule, PanelBarModule, SplitterModule, TabStripModule,
+        ButtonGroupModule, ButtonModule, ButtonsModule,
+        CalendarModule, DatePickerModule, DateRangeModule, TimePickerModule, MultiViewCalendarModule, DateInputModule, DateInputsModule, CalendarsModule
     ],
     exports:[ 
         BrowserAnimationsModule, 
         ToolBarModule, 
         GridModule, BodyModule, ExcelModule, FilterMenuModule, FooterModule, GroupModule, HeaderModule, 
         DropDownsModule,
-        LayoutModule, PanelBarModule, SplitterModule, TabStripModule
+        LayoutModule, PanelBarModule, SplitterModule, TabStripModule,
+        ButtonGroupModule, ButtonModule, ButtonsModule,
+        CalendarModule, DatePickerModule, DateRangeModule, TimePickerModule, MultiViewCalendarModule, DateInputModule, DateInputsModule, CalendarsModule
     ]
 })
 export class WebConsoleUIKitKendoProviderModule {
