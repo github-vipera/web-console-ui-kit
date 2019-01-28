8b106f60cf9a575aedeca2d421166ef803ba882d
Marco Bonati <marco.bonati@vipera.com>
2019-01-28 22:26:23 +0100

    Main Refactoring for common controls (loading overlay)

--- projects/web-console-ui-kit/src/lib/components/grid/index.ts
+++ projects/web-console-ui-kit/src/lib/components/grid/index.ts
@@ -2,3 +2,4 @@ export * from './WCGridModule';
 export * from './grid-editor-command/grid-editor-command-component';
 export * from './grid-editor-commands-group/grid-editor-commands-group-component';
 export * from './edit-service/wc-edit-service';
+export * from './loading-overlay/loading-overlay-component';
